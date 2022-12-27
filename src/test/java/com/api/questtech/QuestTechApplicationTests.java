e0a5e24cdb94dd1f3e75f0d3da968b4f62c1ebe7
Bryan Santos <sp.bryansantos@gmail.com>
2022-12-27 20:35:32 -0300

    Project created

---
+++ src/test/java/com/api/questtech/QuestTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.questtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
